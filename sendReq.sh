77730cbd565fa1547e63ab6928120c06c7104c2a
BossCode45 <human.cyborg42@gmail.com>
2022-07-13 17:22:17 +1200

    Commit for pull

--- sendReq.sh
+++ sendReq.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-Type: application/json" -d @./$1 http://localhost:3000/$2
+curl -XPOST -H "Content-Type: application/json" -d @./$1 http://192.168.0.163:3000/$2
 echo
